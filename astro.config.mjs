0cb29962539b4772c56a989c013b5a13c80fe96e
Debanjan Choudhury <debanjanc01@gmail.com>
2025-09-02 02:50:20 +0530

    Update site URL for correct GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://debanjanc01.github.io/portfolio-web',
+  integrations: [sitemap()],
+  build: {
+    format: 'directory',
+  },
+});
